c32c38109002598d05d17e777a1bed3612c9d2d8
liujie <1411068461@qq.com>
2024-10-30 23:34:41 +0800

    添加 Sass，以及全局 Sass 变量

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,9 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
     <div>
+        <div id="parent">
+            <div id="child">hello</div>
+        </div>
         <a href="https://vite.dev" target="_blank">
             <img src="/vite.svg" class="logo" alt="Vite logo" />
         </a>
@@ -14,7 +17,12 @@ import HelloWorld from './components/HelloWorld.vue'
     <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+#parent {
+    #child {
+        color: $color;
+    }
+}
 .logo {
     height: 6em;
     padding: 1.5em;
